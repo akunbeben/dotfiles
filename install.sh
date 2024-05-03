1a41532659fdcd3adaa30e6f95789c57577e6379
Benny Rahmat <akunbeben@gmail.com>
2024-05-04 02:23:46 +0800

    generate ssh keys

--- install.sh
+++ install.sh
@@ -17,3 +17,15 @@ command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $(whoami)
 
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
+ssh-keygen -t ed25519 -C "akunbeben@gmail.com" -f "$HOME/.ssh/github_personal" <<< ""
+eval "$(ssh-agent -s)"
+ssh-add "$HOME/.ssh/github_personal"
+
+ssh-keygen -t ed25519 -C "akunbeben@gmail.com" -f "$HOME/.ssh/gitlab_personal" <<< ""
+eval "$(ssh-agent -s)"
+ssh-add "$HOME/.ssh/gitlab_personal"
+
+ssh-keygen -t ed25519 -C "benny.rahmat@thrive.co.id" -f "$HOME/.ssh/gitlab_work" <<< ""
+eval "$(ssh-agent -s)"
+ssh-add "$HOME/.ssh/gitlab_work"
